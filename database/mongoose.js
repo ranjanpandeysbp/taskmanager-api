a0378fa1c35b2cf69c999442661944003633645f
Ranjan Pandey <ranjanpandey2006@hotmail.com>
2021-09-26 13:16:59 +0000

    Updated mongoose.js

--- database/mongoose.js
+++ database/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-mongoose.connect('mongodb://rpmeanstackdb:aYMuXWKVzWh2Zas7tBgIm2FMYvaOjSnqcfsZ2rMUNQi2Kirp2H5r2H5eEuJ4NrWR2vyygXRAIXuzyNszUiJ8tw==@rpmeanstackdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@rpmeanstackdb@', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect('mongodb://rpmeanstackdb:A97Z7boRTvhD78nyjDbTvX76wJEMKREL8byqpfgdwdufqSJLcrhJjOI2JvQNEM0zE0w8c1ApNpsURQulJBHZuA==@rpmeanstackdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@rpmeanstackdb@', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => {
         console.log("DB Connected Sucessfully!")
     })
